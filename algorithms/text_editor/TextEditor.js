36586f56029d213aef25b8a4ec4383d238982a0f
Akyuna Akish <akyunaakish@gmail.com>
2019-02-24 22:00:29 -0700

    added undo initial functionality

--- algorithms/text_editor/TextEditor.js
+++ algorithms/text_editor/TextEditor.js
@@ -3,14 +3,30 @@ const commands = require('./commands');
 class TextEditor {
     constructor() {
         this.s = '';
+        this.history = [];
+        this.historyPosition = -1;
     }
 
     insert(str) {
+        let prev = this.s;
         this.s += str;
+        this.history.push({ type: commands.INSERT, prev, next: this.s });
+        this.historyPosition += 1;
     }
 
     delete() {
+        let prev = this.s;
         this.s = this.s.slice(0, this.s.length - 1);
+        this.history.push({ type: commands.DELETE, prev, next: this.s });
+    }
+
+    undo() {
+        if(this.historyPosition == 0) {
+            this.s = '';
+        } else {
+            this.historyPosition -= 1;
+            this.s = this.history[this.historyPosition].next;
+        }
     }
 
     input(command) {
@@ -21,6 +37,10 @@ class TextEditor {
 
             case commands.DELETE:
                 this.delete();
+            break;
+
+            case commands.UNDO:
+                this.undo();
             break;
         }
     }
--- algorithms/text_editor/TextEditor.test.js
+++ algorithms/text_editor/TextEditor.test.js
@@ -33,3 +33,57 @@ test(`TextEditor can INSERT, then DELETE an input and print it`, () => {
 
     expect(Editor.print()).toBe(valAfterDelete);
 });
+
+
+test(`TextEditor can INSERT, then UNDO an input and print it`, () => {
+    const command = {
+        type: commands.INSERT,
+        value: 'YOLO'
+    };
+
+    const command2 = {
+        type: commands.UNDO
+    };
+
+    let Editor = new TextEditor();
+
+    Editor.input(command);
+    Editor.input(command2);
+
+    expect(Editor.print()).toBe('');
+});
+
+test(`TextEditor can INSERT multiple, then UNDO multiple and print the final result`, () => {
+    const command = {
+        type: commands.INSERT,
+        value: 'YOLO'
+    };
+
+    const command2 = {
+        type: commands.INSERT,
+        value: ' BROHA'
+    };
+
+    const command3 = {
+        type: commands.INSERT,
+        value: ' ALOHA'
+    };
+
+    const command4 = {
+        type: commands.UNDO
+    };
+
+    const command5 = {
+        type: commands.UNDO
+    };
+
+    let Editor = new TextEditor();
+
+    Editor.input(command);
+    Editor.input(command2);
+    Editor.input(command3);
+    Editor.input(command4);
+    Editor.input(command5);
+
+    expect(Editor.print()).toBe(command.value);
+});
